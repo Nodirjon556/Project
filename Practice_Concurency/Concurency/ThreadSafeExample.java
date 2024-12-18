f39418a8aab498806f9843885f1219a7f06a8bcc
Nodirjon Okilov <nodiroqilov86@gmail.com>
2024-12-18 22:06:07 +0900

    new

--- Practice_Concurency/Concurency/ThreadSafeExample.java
+++ Practice_Concurency/Concurency/ThreadSafeExample.java
@@ -2,11 +2,38 @@ package Concurency;
 
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 public class ThreadSafeExample {
-    public static void main(String[] args) {
-        var set1 = new HashMap<Integer>();
-        var set2 = Collections.synchronizedSet(set1);
+    public static void main(String[] args) throws InterruptedException {
+        var set = ConcurrentHashMap.<Integer>newKeySet();
+        Runnable runnable1 = () -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    set.add(i);
+                }
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+
+            }
+        };
+
+        Runnable runnable2 = () -> {
+            try {
+                for (int i = 0; i < 10; i++) {
+                    set.add(i);
+                }
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+
+            }
+        };
+
+        new Thread(runnable1).start();
+        new Thread(runnable2).start();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(set.size());
 
     }
 }

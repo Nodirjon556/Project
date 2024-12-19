4c1a715551e1daa6d78290ffe408fd718589c040
Nodirjon Okilov <nodiroqilov86@gmail.com>
2024-12-19 15:34:39 +0900

    new

--- Practice_Concurency/Code_uz/IO_NIOpackage/Main.java
+++ Practice_Concurency/Code_uz/IO_NIOpackage/Main.java
@@ -5,11 +5,17 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File file = new File("ikki.txt");
-       // System.out.println(file.exists());
-        if (!file.exists()){
-           boolean answer =  file.createNewFile();
+        File file = new File("123.txt");
+       // System.out.println(file.exists());  // check
+
+        /*if (!file.exists()){
+           boolean answer =  file.createNewFile();  // create a file()
             System.out.println(answer);
-        }
+        }*/
+
+        /*boolean res = file.delete();      // delete()
+        System.out.println(res);*/
+
+        System.out.println(file.isFile());
     }
 }

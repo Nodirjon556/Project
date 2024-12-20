463e60823910a46f8f23cafa3eead8aeda396218
Nodirjon Okilov <nodiroqilov86@gmail.com>
2024-12-20 19:45:57 +0900

    done

---
+++ Practice_Concurency/Code_uz/IO_NIOpackage/Exercises/Exercise3.java
@@ -0,0 +1,31 @@
+package Code_uz.IO_NIOpackage.Exercises;
+
+import java.io.IOException;
+import java.io.*;
+
+public class Exercise3 {
+    public static void main(String[] args) throws IOException {
+        // test folder dagi file larni ma'lumotlarini o'qib output.txt ga yozing
+
+        PrintWriter printWriter = new PrintWriter("output.txt");
+
+        File file = new File("test");
+
+        String[] list = file.list();
+
+        for (String s : list) {
+            File subFile = new File("test/" + s);
+            if (subFile.isFile()) {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(subFile));
+                String line;
+
+                while ((line = bufferedReader.readLine())!= null){
+                    printWriter.println(line);
+                }
+                bufferedReader.close();
+            }
+        }
+        printWriter.flush();
+        printWriter.close();
+    }
+}
---
+++ Practice_Concurency/Code_uz/IO_NIOpackage/Exercises/Exercise4.java
@@ -0,0 +1,9 @@
+package Code_uz.IO_NIOpackage.Exercises;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        // a.txt file dan b.txt file da bor bo'lgan qatorlarni o'chiring
+
+
+    }
+}
